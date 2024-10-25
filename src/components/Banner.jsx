491b8a10bff6e574bda4118d599b32dea3badcba
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2024-10-25 18:56:31 +0530

    change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,19 +3,25 @@ import ImageSlider from './ImageSlider';
 
 const Banner = () => {
   return (
-    <section className='h-[100vh] p-10 bg-[url(./assets/banner.png)] bg-opacity-100 bg-center relative' >
-      <div className='absolute inset-0 mx-7 md:mx-auto md:container  flex'>
-         <div className='flex flex-col lg:flex-row gap-10 items-center justify-center w-full'>
+    <section 
+      className="relative min-h-screen bg-[url('./assets/banner.png')] bg-cover bg-center flex items-center pb-10 lg:pb-0" >
 
-        <div className='lg:w-1/2 text-white order-2 lg:order-1 flex flex-col gap-10'>
-            <h1 className='text-6xl text-[#ffffff] font-bold'>PENSAKI BLACKBOARD</h1>
-            <p className='text-[33px]'>Efficient, Reliable, and Effortless: Unlock the Potential of Pensaki.org Software Solution !</p>
+      <div className="flex flex-col mx-5 md:container md:mx-auto lg:flex-row gap-10 items-center justify-center w-full relative z-10 ">
+        {/* Text content */}
+        <div className="lg:w-1/2 text-white order-2 lg:order-1 flex flex-col gap-6">
+          <h1 className="text-4xl lg:text-6xl font-bold py-0 lg:py-3">PENSAKI BLACKBOARD</h1>
+          <p className="text-2xl lg:text-[30px] py-0 lg:py-3 leading-loose">
+            Efficient, Reliable, and Effortless: Unlock the Potential of Pensaki.org Software Solution!
+          </p>
+          <div className='flex gap-5'>
+            <a className='lg:px-10 py-4 border-2 px-5 rounded-lg hover:bg-white hover:text-black transition-all duration-700' href="">Our Products</a>
+            <a className='lg:px-10 py-4 border-2 px-5 rounded-lg bg-white text-black hover:bg-transparent hover:text-white transition-all duration-700' href="">Global Support</a>
+          </div>
         </div>
 
-        {/* ImageSlider should appear first on mobile view */}
-        <div className='lg:w-1/2 order-1 lg:order-2'>
-            <ImageSlider />
-        </div>
+        {/* Image Slider component */}
+        <div className="lg:w-1/2 order-1 lg:order-2">
+          <ImageSlider />
         </div>
       </div>
     </section>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,7 +1,188 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { BiChevronDown, BiChevronRight, BiChevronUp } from "react-icons/bi";
+import { CgClose } from "react-icons/cg";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { MdArrowOutward } from "react-icons/md";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(false);
+  const [showChild, setShowChild] = useState("");
+  // Function to handle scroll event
+  const handleScroll = () => {
+    setShowNav(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const MenuItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name : "About us",
+      child: [
+        {
+          name: "Cookie policy",
+          link: "/cookie"
+        },
+        {
+          name: "Privacy policy",
+          link: "/privacy"
+        },
+        {
+          name: "General Condition",
+          link: "/general"
+        }
+      ]
+    },
+    {
+      name: "Features",
+      link: "features"
+    },
+    {
+      name: "Products",
+      link: "products"
+    },
+    {
+      name: "FAQ?",
+      link: "/faq",
+    },
+    {
+      name: "Global support",
+      link: "/support",
+    },
+  ];
   return (
-    <div>Navbar</div>
-  )
+    <nav className="sticky top-0 shadow-lg shadow-black bg-white z-50">
+      <section className="py-2.5 mx-5 md:container md:mx-auto flex justify-between items-center">
+        {/* logo here  */}
+        <Link to={"/"} className="flex items-center">
+          <img src={logo} className="h-[50px] md:h-[70px]" alt="" />
+        </Link>
+        {/* desktop view  */}
+        <div className="hidden lg:flex lg:gap-8 lg:items-center">
+          {MenuItems.map((mi, i) => (
+            <div key={i}>
+              {mi.child ? (
+                <div className="relative group text-[18px]">
+                  <span className="flex items-center gap-1 cursor-pointer font-semibold">
+                    {mi.name}
+                    <BiChevronDown className="text-2xl" />
+                  </span>
+                  <div className="absolute bg-white left-5 p-5 shadow rounded min-w-[420px] hidden group-hover:flex flex-col gap-2">
+                    {mi.child.map((mc, i) => (
+                      <Link to={mc.link} key={i} className="flex gap-1.5 hover:font-semibold hover:translate-x-3 hover:text-primary duration-300 ease-linear">
+                        {/* <BiChevronRight className="text-2xl" /> */}
+                        <span className="flex-1">{mc.name}</span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <Link
+                  to={mi.link}
+                  key={i}
+                  className="text-[18px] font-semibold flex items-center gap-2.5"
+                >
+                  {mi.name}
+                  {/* <MdArrowOutward /> */}
+                </Link>
+              )}
+            </div>
+          ))}
+        </div>
+        {/* mobile view  */}
+        {showNav ? (
+          <button className="lg:hidden" onClick={() => setShowNav(!showNav)}>
+            <CgClose className="text-2xl" />
+          </button>
+        ) : (
+          <button className="lg:hidden" onClick={() => setShowNav(!showNav)}>
+            <FaBars className="text-xl" />
+          </button>
+        )}
+        {showNav && (
+          <div className="lg:hidden p-5 md:px-14 flex flex-col gap-4 absolute top-16  left-0 bg-white min-w-full h-80">
+            {MenuItems.map((mi, i) => (
+              <div key={i}>
+                {mi.child ? (
+                  <div>
+                    <div className="text-[18px] flex justify-between items-center">
+                      {mi.name}
+
+                      {showChild !== i ? (
+                        <button
+                          onClick={() => setShowChild(i)}
+                          className="flex items-center gap-1 cursor-pointer"
+                        >
+                          <BiChevronDown className="text-3xl" />
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => setShowChild("")}
+                          className="flex items-center gap-1 cursor-pointer"
+                        >
+                          <BiChevronUp className="text-3xl" />
+                        </button>
+                      )}
+                    </div>
+
+                    {showChild === i && (
+                      <div className="ml-4 mt-4 flex flex-col gap-4">
+                        {mi.child.map((mc, i) => (
+                          <Link
+                            to={mc.link}
+                            key={i}
+                            onClick={() => setShowNav(!showNav)}
+                            className="text-[18px] flex gap-1.5"
+                          >
+                            <BiChevronRight className="text-2xl" />
+                            {mc.name}
+                          </Link>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <Link
+                    to={mi.link}
+                    key={i}
+                    onClick={() => setShowNav(!showNav)}
+                    className="text-[18px] flex justify-between items-center"
+                  >
+                    {mi.name}
+                    {/* <MdArrowOutward /> */}
+                  </Link>
+                )}
+              </div>
+            ))}
+            <Link
+              to={"/contact"}
+              onClick={() => setShowNav(!showNav)}
+              className="text-[18px] flex justify-between items-center"
+            >
+              Contact Us
+              {/* <MdArrowOutward /> */}
+            </Link>
+          </div>
+        )}
+        {/* desktop mode contact bbutton */}
+        <Link
+          to={"/contact"}
+          className="px-4 py-2 bg-primary rounded shadow text-white hidden lg:flex gap-2.5 items-center group h-fit"
+        >
+          Contact{" "}
+          <MdArrowOutward className="group-hover:rotate-45 duration-300 ease-linear" />
+        </Link>
+      </section>
+      <div className="bg-primary w-full h-0.5"></div>
+    </nav>
+  );
 }
